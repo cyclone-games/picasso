850e72345e91dbc6fadbf68eed07f6c721d21c20
Nick Dugger <email@nickdugger.com>
2018-05-30 12:42:01 -0500

    Update Shader.js

--- core/Shader.js
+++ core/Shader.js
@@ -1,9 +1,15 @@
+const attributeRegex = /attribute .+? (.+?);/;
+const uniformRegex = /uniform .+? (.+?);/;
+
 export default class Shader {
     
     static types = {
         fragment: 'fragment',
         vertex: 'vertex'
     };
+
+    attributes = [ ];
+    uniforms = [ ];
     
     constructor (type, glsl) {
         
@@ -11,16 +17,25 @@ export default class Shader {
             throw new Error('Invalid shader type');
         }
         
-        this.type = type;
+        this.compiled = null;
         this.glsl = glsl;
+        this.type = type;
     }
 
-    compile (context) {
-        const shader = context.createShader(`${ this.type.toUpperCase() }_SHADER`);
+    compile (renderer) {
+        const shader = renderer.context.createShader(`${ this.type.toUpperCase() }_SHADER`);
         
-        context.shaderSource(shader, this.glsl);
-        context.compileShader(shader);
+        renderer.context.shaderSource(shader, this.glsl);
+        renderer.context.compileShader(shader);
         
-        return shader;
+        for (const attribute of this.glsl.match(new RegExp(attributeRegex, 'g'))) {
+            this.attributes.push(attribute.match(attributeRegex)[ 1 ]);
+        }
+        
+        for (const uniform of this.glsl.match(new RegExp(uniformRegex, 'g'))) {
+            this.uniforms.push(uniform.match(uniformRegex)[ 1 ]);
+        }
+        
+        this.compiled = shader;
     }
 }
