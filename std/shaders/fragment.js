f4118e193941aaa94e66167bd6710eff0a6d6164
Nick Dugger <email@nickdugger.com>
2018-05-30 10:07:36 -0500

    Update fragment.js

--- std/shaders/fragment.js
+++ std/shaders/fragment.js
@@ -1,1 +1,12 @@
+import Shader from '../core/Shader';
 
+export default new Shader(Shader.types.fragment, `
+    precision mediump float;
+
+    uniform sampler2D u_image;
+    varying vec2 v_textureCoords;
+
+    void main() {
+        gl_FragColor = texture2D(u_image, v_textureCoords);
+    }
+`);
