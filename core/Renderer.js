daa5c6fe79c8f591a96759d5b4a102b7dbcb6a6a
Nick Dugger <email@nickdugger.com>
2018-05-30 14:53:41 -0500

    Update Renderer.js

--- core/Renderer.js
+++ core/Renderer.js
@@ -18,21 +18,48 @@ export default class Renderer {
             this.gl.attachShader(this.program, compiled);
             
             for (const attribute of attributes) {
-                this.attributes[ attribute.name ] = this.gl.getAttribLocation(this.program, attribute.name);
+                this.attributes[ attribute.name ] = {
+                    location: this.gl.getAttribLocation(this.program, attribute.name),
+                    size: attribute.size
+                };
+                
                 this.buffers[ attribute.name ] = this.gl.createBuffer();
                 
-                this.gl.enableVertexAttribArray(this.attributes[ attribute.name ]);
+                this.gl.enableVertexAttribArray(this.attributes[ attribute.name ].location);
             }
             
             for (const uniform of uniforms) {
-                this.uniforms[ uniform.name ] = this.gl.getUniformLocation(this.program, uniform.name);
+                this.uniforms[ uniform.name ] = {
+                    location: this.gl.getUniformLocation(this.program, uniform.name),
+                    size: uniform.size
+                };
             }
         }
         
         this.gl.useProgram(this.program);
     }
+
+    setAttribute (name, ...values) {
+        const attribute = this.attributes[ name ];
+              
+        if (values.length !== attribute.size) {
+            throw new Error(`Invalid number of values for ${ name }: Expected ${ attribute.size }, found ${ values.length }`);
+        }
+        
+        // TODO
+    }
+
+    setUniform (name, ...values) {
+        const uniform = this.uniforms[ name ];
+              
+        if (values.length !== uniform.size) {
+            throw new Error(`Invalid number of values for ${ name }: Expected ${ uniform.size }, found ${ values.length }`);
+        }
+        
+        // TODO
+    }
     
     render (renderable) {
-        
+        // TODO
     }
 }
