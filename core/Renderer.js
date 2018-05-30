7f67b88580df5ba6986da81b2d65d3af6bbbc3d1
Nick Dugger <email@nickdugger.com>
2018-05-30 10:22:50 -0500

    Update Renderer.js

--- core/Renderer.js
+++ core/Renderer.js
@@ -1,1 +1,11 @@
-
+export default class Renderer {
+    
+    constructor (canvas) {
+        this.canvas = canvas;
+        this.context = canvas.getContext('webgl');
+    }
+    
+    render (renderable) {
+        
+    }
+}
