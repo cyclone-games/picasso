2265a0e83ef85b43f9fb4233e05c5f25188c766e
Nick Dugger <email@nickdugger.com>
2018-05-30 12:52:44 -0500

    Update Renderer.js

--- core/Renderer.js
+++ core/Renderer.js
@@ -1,9 +1,31 @@
 export default class Renderer {
     
+    attributes = { };
+    uniforms = { };
+    
     constructor (canvas) {
         this.canvas = canvas;
-        this.context = this.canvas.getContext('webgl2');
+        this.gl = this.canvas.getContext('webgl2');
         this.program = this.context.createProgram();
+    }
+    
+    initialize (shaders) {
+        
+        for (const shader of shaders) {
+            const { attributes, compiled, uniforms } = shader.compile(this.gl);
+            
+            this.gl.attachShader(this.program, compiled);
+            
+            for (const attribute of attributes) {
+                this.attributes[ attribute ] = this.gl.getAttribLocation(this.program, attribute);
+            }
+            
+            for (const uniform of uniforms) {
+                this.uniforms[ uniform ] = this.gl.getUniformLocation(this.program, uniform);
+            }
+        }
+        
+        this.gl.linkProgram(this.program);
     }
     
     render (renderable) {
