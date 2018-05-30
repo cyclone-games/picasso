d1b26f6dc1b83bf53917b1778f413717ee06eb88
Nick Dugger <email@nickdugger.com>
2018-05-30 10:04:44 -0500

    Update Shader.js

--- core/Shader.js
+++ core/Shader.js
@@ -1,1 +1,26 @@
+export default class Shader {
+    
+    static types = {
+        fragment: 'fragment',
+        vertex: 'vertex'
+    };
+    
+    constructor (type, glsl) {
+        
+        if (!(type in Shader.types)) {
+            throw new Error('Invalid shader type');
+        }
+        
+        this.type = type;
+        this.glsl = glsl;
+    }
 
+    compile (context) {
+        const shader = context.createShader(`${ this.type.toUpperCase() }_SHADER`);
+        
+        context.shaderSource(shader, this.glsl);
+        context.compileShader(shader);
+        
+        return shader;
+    }
+}
