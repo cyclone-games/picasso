06403db46b351bbc6478a51057a7ea747f0b8d4e
Nick Dugger <email@nickdugger.com>
2018-06-01 08:43:34 -0500

    Update Program.js

--- core/Program.js
+++ core/Program.js
@@ -1,34 +1,33 @@
 export default class Program {
 
-    constructor (gl, shaders) {
-        this.gl = gl;
+    constructor (shaders) {
         this.program = null;
         this.shaders = shaders;
     }
 
-    compile () {
-        this.program = this.gl.createProgram();
-        this.shaders = this.shaders.map(shader => shader.compile(this.gl));
+    compile (gl) {
+        this.program = gl.createProgram();
+        this.shaders = this.shaders.map(shader => shader.compile(gl));
 
         for (const { compiled } of this.shaders) {
-            this.gl.attachShader(this.program, compiled);
-            this.gl.linkProgram(this.program);
+            gl.attachShader(this.program, compiled);
+            gl.linkProgram(this.program);
         }
 
         for (const { attributes, uniforms } of this.shaders) {
 
             for (const attribute of attributes) {
-                attribute.location = this.gl.getAttribLocation(this.program, attribute.id);
+                attribute.location = gl.getAttribLocation(this.program, attribute.id);
             }
 
             for (const uniform of uniforms) {
-                uniform.location = this.gl.getUniformLocation(this.program, uniform.id);
+                uniform.location = gl.getUniformLocation(this.program, uniform.id);
             }
         }
     }
 
-    use () {
-        this.gl.useProgram(this.program);
+    use (gl) {
+        gl.useProgram(this.program);
     }
 
     getAttribute (id) {
