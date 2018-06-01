dcb91af724813ac046f1d798af3a449346b3451f
Nick Dugger <email@nickdugger.com>
2018-06-01 08:57:21 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import Shader from './core/shader';
+import WebGL from './core/Renderer;
+
+export {
+    Shader,
+    WebGL
+}
+
+export default function picasso (canvas) {
+    return new WebGL(canvas, 2);
+}
