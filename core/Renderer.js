b0bd192155d4765d439bc700a49b302d3b4cba69
Nick Dugger <email@nickdugger.com>
2018-06-01 08:54:16 -0500

    Update Renderer.js

--- core/Renderer.js
+++ core/Renderer.js
@@ -2,21 +2,21 @@ import Program from './Program';
 
 export default class Renderer {
 
-    constructor (canvas) {
+    constructor (canvas, v = 2) {
         this.canvas = canvas;
-        this.gl = this.canvas.getContext('webgl2');
+        this.context = canvas.getContext(`webgl${ v > 1 ? v : '' }`);
         this.programs = { };
         this.using = null;
     }
 
     initialize (id, shaders) {
         this.programs[ id ] = new Program(shaders);
-        this.programs[ id ].compile(this.gl);
+        this.programs[ id ].compile(this.context);
         this.useProgram(id);
     }
 
     useProgram (id) {
-        this.programs[ id ].use(this.gl);
+        this.programs[ id ].use(this.context);
         this.using = id;
     }
 
@@ -24,20 +24,20 @@ export default class Renderer {
         const attribute = this.programs[ this.using ].getAttribute(id);
 
         if (!attribute) {
-            throw new Error(`Unable to set attribute: No attribute named ${ id } exists in the current program`);
+            throw new Error(`Unable to set attribute: No attribute named "${ id }" exists in the current program`);
         }
 
-        attribute.set(this.gl, values);
+        attribute.set(this.context, values);
     }
 
     setUniform (id, values) {
         const uniform = this.programs[ this.using ].getUniform(id);
 
         if (!uniform) {
-            throw new Error(`Unable to set uniform: No uniform named ${ id } exists in the current program`);
+            throw new Error(`Unable to set uniform: No uniform named "${ id }" exists in the current program`);
         }
 
-        uniform.set(this.gl, values);
+        uniform.set(this.context, values);
     }
 
     render (renderable) {
