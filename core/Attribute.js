cf3f2898b6248b6ef260e8d1a829a9e95345866c
Nick Dugger <email@nickdugger.com>
2018-06-01 08:45:49 -0500

    Update Attribute.js

--- core/Attribute.js
+++ core/Attribute.js
@@ -2,22 +2,21 @@ export default class Attribute {
 
     static regex = /attribute (.+? )?(.+?) (.+?);/;
 
-    constructor (buffer, gl, id, size, type) {
+    constructor (buffer, id, size, type) {
         this.buffer = buffer;
-        this.gl = gl;
         this.id = id;
         this.location = null;
         this.size = size;
         this.type = type;
     }
 
-    enable () {
-        this.gl.enableVertexAttribArray(this.location);
+    enable (gl) {
+        gl.enableVertexAttribArray(this.location);
     }
 
-    set (values) {
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer);
-        this.gl.vertexAttribPointer(
+    set (gl, values) {
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+        gl.vertexAttribPointer(
             this.location,
             this.size,
             this.gl.FLOAT,
@@ -25,10 +24,10 @@ export default class Attribute {
             0,
             0
         );
-        this.gl.bufferData(
-            this.gl.ARRAY_BUFFER,
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
             new Float32Array(values),
-            this.gl.STATIC_DRAW
+            gl.STATIC_DRAW
         );
     }
 }
